067846701a446e2ac2fa7ab2042efd276233082d
zeriyoshi <zeriyoshi@gmail.com>
2018-08-28 00:54:27 +0900

    refactoring readlink_f

--- example/example.sh
+++ example/example.sh
@@ -3,12 +3,8 @@
 readlink_f()
 {
     (
-        if [ "${1}" = "" ]; then
-            return 1
-        fi
-
         TARGET_PATH="${1}"
-
+        if [ "${TARGET_PATH}" = "" ]; then return 1; fi
         while [ -L "${TARGET_PATH}" ]; do
             READLINK_PATH="$(readlink "${TARGET_PATH}")"
             TARGET_PATH="$(cd "$(dirname "${TARGET_PATH}")"; cd "$(dirname "${READLINK_PATH}")"; pwd)/$(basename "${READLINK_PATH}")"
