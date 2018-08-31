4ba55755f2941a8f1d275e455b420f3d5733432f
zeriyoshi <zeriyoshi@gmail.com>
2018-08-31 18:05:40 +0900

    fix sub command parsing

--- framework/framework.sh
+++ framework/framework.sh
@@ -165,7 +165,7 @@ coal_framework_run()
 
     while [ "${#}" -gt "0" ]; do
         if coal_command_exists "${___COAL_FRAMEWORK_RUN_NAMESPACE}" "${@}"; then
-            coal_parser_parse "${___COAL_FRAMEWORK_RUN_NAMESPACE}" "${@}"
+            coal_parser_parse "${@}"
             coal_command_call "${___COAL_FRAMEWORK_RUN_NAMESPACE}" "${@}"
         fi
         shift
